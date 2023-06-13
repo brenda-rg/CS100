e7792c4ad566ae1afaecaf4dfcfcfcfb9f63ab70
brenda-rg <broja016@ucr.edu>
2023-06-12 19:13:37 -0700

    add sort files

---
+++ header/UserSort.h
@@ -0,0 +1,20 @@
+#ifndef FINAL_PROJECT_BROJA016_AOROZ064_ANGUY344_IKALU001_USERSORT_H
+#define FINAL_PROJECT_BROJA016_AOROZ064_ANGUY344_IKALU001_USERSORT_H
+
+//also include sql class
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include "../header/UserSort.h"
+#include "../header/Task.h"
+
+using namespace std;
+
+class UserSort : public UserBase {
+	void getTaskByAlpha(TaskList&);
+    void getTaskChrono(TaskList&);
+    void getTaskByPriority(TaskList&);
+};
+
+
+#endif //FINAL_PROJECT_BROJA016_AOROZ064_ANGUY344_IKALU001_USERSORT_H
---
+++ src/UserSort.cpp
@@ -0,0 +1,55 @@
+#ifndef FINAL_PROJECT_BROJA016_AOROZ064_ANGUY344_IKALU001_USERSORT_CPP
+#define FINAL_PROJECT_BROJA016_AOROZ064_ANGUY344_IKALU001_USERSORT_CPP
+
+//also include sql class
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include "../header/UserSort.h"
+#include "../header/TaskList.h"
+
+using namespace std;
+
+void UserSort::getTaskByAlpha(TaskList& tasklist) {
+    for(int i = 0; i < tasklist.size(); i++) {
+        for(int j = i+1; j < tasklist.size(); j++) {
+            if(tasklist.at(i)->get_name() > taskList.at(j)->get_name()) {
+                Task* temp = tasklist.at(i);
+                tasklist.at(i) = tasklist.at(j);
+                tasklist.at(j) = temp;
+                temp = nullptr;
+            }
+        }
+    }
+
+}
+
+//needs fixing
+void UserSort::getTaskChrono(TaskList tasklist ) {
+    for(int i = 0; i < tasklist.size(); i++) {
+        for(int j = i+1; j < tasklist.size(); j++) {
+            if(tasklist.at(i)->get_date() > taskList.at(j)->get_date()) {
+                Task* temp = tasklist.at(i);
+                tasklist.at(i) = tasklist.at(j);
+                tasklist.at(j) = temp;
+                temp = nullptr;
+            }
+        }
+    }
+}
+
+void UserSort::getTaskByPriority(TaskList& tasklist ) {
+    for(int i = 0; i < tasklist.size(); i++) {
+        for(int j = i+1; j < tasklist.size(); j++) {
+            if((tasklist.at(i)->get_priority() > taskList.at(j)->get_priority()) && (tasklist.at(i) != -1 && tasklist.at(j) != -1)) {
+                Task* temp = tasklist.at(i);
+                tasklist.at(i) = tasklist.at(j);
+                tasklist.at(j) = temp;
+                temp = nullptr;
+            }
+        }
+    }
+}
+
+
+#endif //FINAL_PROJECT_BROJA016_AOROZ064_ANGUY344_IKALU001_USERSORT_CPP
